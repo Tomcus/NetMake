36ca0006bd00929aea6e3f025bb2e22b6a5df569
Tomas Vybiral <tomas@vybiral.me>
2021-02-07 21:24:27 +0100

    Reverted to using strings

--- src/netmake/generation/generate.cpp
+++ src/netmake/generation/generate.cpp
@@ -7,82 +7,22 @@
 #include <sstream>
 #include <stdexcept>
 #include <filesystem>
-#include <memory>
-#include <cstring>
+#include <string>
 
 using namespace netmake;
 using namespace std::filesystem;
 
-class text_obj {
-    char * ptr{nullptr};
-    size_t curr_size{0};
-    size_t curr_capacity{0};
-    const size_t INITIAL_CAPACITY{128};
-protected:
-    void resize() {
-        const auto old_data = ptr;
-
-        if (curr_capacity == 0) {
-            curr_capacity = INITIAL_CAPACITY;
-        } else {
-            curr_capacity *= 2;
-        }
-
-        ptr = new char[curr_capacity + 1];
-
-        if (old_data != nullptr) {
-            memcpy(ptr, old_data, curr_size);
-        }
-    }
-
-    void append_raw(const char* data, size_t size) {
-        const auto new_size = curr_size + size;
-        
-        if (new_size > curr_capacity) {
-            resize();
-        }
-
-        if (size > 0) {
-            memcpy(ptr + curr_size, data, size);
-        }
-        curr_size += size;
-        if (ptr != nullptr) {
-            ptr[curr_size] = 0;
-        }
-    }
-public:
-    const char* data() const {
-        if (ptr == nullptr) 
-            return "";
-        return ptr;
-    }
-
-    size_t size() const {
-        return curr_size;
-    }
-
-    void append(const std::string& str) {
-        append_raw(str.data(), str.size());
-    }
-
-    void append(const std::shared_ptr<text_obj>& text) {
-        append_raw(text->data(), text->size());
-    }
-};
-
-using text = std::shared_ptr<text_obj>;
-text create_text() { return std::make_shared<text_obj>(); }
-
-text load_file(const path& path) {
-    text res = create_text();
-    std::ostringstream oss{};
+std::string load_file(const path& path) {
     std::ifstream ifs(path);
     if (!ifs.good()) {
-        throw std::runtime_error(fmt::format("Can't open/find file {}", path.c_str()));
+        throw std::runtime_error(fmt::format("File can't be open/doesn't exist. Path: {}", path.c_str()));
     }
+    std::ostringstream oss;
     oss << ifs.rdbuf();
-    res->append(oss.str());
-    return res;
+    if (!ifs.good()) {
+        throw std::runtime_error(fmt::format("Error while reading file: {}", path.c_str()));
+    }
+    return oss.str();
 }
 
 json load_json(const path& path) {
@@ -114,16 +54,16 @@ void netmake::generate() {
     }
 }
 
-text get_meta_tag(const std::string& meta_tag_name, const json& meta_data) {
-    text res = std::make_shared<text_obj>();
+std::string get_meta_tag(const std::string& meta_tag_name, const json& meta_data) {
+    std::string res = "";
     if (meta_data.contains(meta_tag_name)) {
-        res->append(fmt::format("<meta name=\"{}\" content=\"{}\" />\n", meta_tag_name, meta_data[meta_tag_name]));
+        res += fmt::format("<meta name=\"{}\" content=\"{}\" />\n", meta_tag_name, meta_data[meta_tag_name]);
     }
     return res;
 }
 
-text generate_keyword_meta_tag(const json& meta_data, const json& site_data) {
-    text res = create_text();
+std::string generate_keyword_meta_tag(const json& meta_data, const json& site_data) {
+    std::string res = "";
     std::vector<std::string> keywords;
     if (meta_data.contains("keywords")) {
         auto key_data = meta_data["keywords"];
@@ -144,24 +84,24 @@ text generate_keyword_meta_tag(const json& meta_data, const json& site_data) {
         }
     }
     if (keywords.size() > 0)
-        res->append(fmt::format("<meta name=\"keywords\" content=\"{}\" />\n", fmt::join(keywords, ", ")));
+        res += fmt::format("<meta name=\"keywords\" content=\"{}\" />\n", fmt::join(keywords, ", "));
     return res;
 }
 
-text generate_extra_styles(const json& site_data) {
-    text res = create_text();
+std::string generate_extra_styles(const json& site_data) {
+    std::string res = "";
     if (site_data.contains("extra_css")) {
         auto extra_styles = site_data["extra_css"];
         for (auto style: extra_styles) {
-            res->append(fmt::format("<link rel=\"stylesheet\" href=\"{}\" />", style));
+            res += fmt::format("<link rel=\"stylesheet\" href=\"{}\" />", style);
         }
     }
     return res;
 }
 
-text generate_header(const json& site_data, const std::string& extra_title = "") {
+std::string generate_header(const json& site_data, const std::string& extra_title = "") {
     static json meta_data = {};
-    static text header_template = load_file(get_template_path("header.html"));
+    static std::string header_template = load_file(get_template_path("header.html"));
     if (meta_data == json{}) {
         std::ifstream md_file(get_source_path("metadata.json"));
         md_file >> meta_data;
@@ -172,25 +112,25 @@ text generate_header(const json& site_data, const std::string& extra_title = "")
         complete_title = fmt::format("{} - {}", extra_title, complete_title);
     }
 
-    text meta_tags = create_text();
-    meta_tags->append("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\" />\n");
+    std::string meta_tags = "";
+    meta_tags += "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\" />\n";
 
-    meta_tags->append("<meta name=\"generator\" content=\"NetMake - TODO: Add url\" />\n");
-    meta_tags->append(get_meta_tag("description", meta_data));
-    meta_tags->append(get_meta_tag("author", meta_data));
-    meta_tags->append(get_meta_tag("application-name", meta_data));
-    meta_tags->append(generate_keyword_meta_tag(meta_data, site_data));
+    meta_tags += "<meta name=\"generator\" content=\"NetMake - TODO: Add url\" />\n";
+    meta_tags += get_meta_tag("description", meta_data);
+    meta_tags += get_meta_tag("author", meta_data);
+    meta_tags += get_meta_tag("application-name", meta_data);
+    meta_tags += generate_keyword_meta_tag(meta_data, site_data);
     
-    text res = create_text();
-    res->append(fmt::format(header_template->data(), fmt::arg("title", complete_title),
-                                                     fmt::arg("meta_tags", meta_tags->data()),
-                                                     fmt::arg("extra_styles", generate_extra_styles(site_data)->data())));
+    std::string res = "";
+    res += fmt::format(header_template, fmt::arg("title", complete_title),
+                                        fmt::arg("meta_tags", meta_tags),
+                                        fmt::arg("extra_styles", generate_extra_styles(site_data)));
     return res;
 }
 
-text generate_nav(const std::string_view& site_name, const json& sites) {
-    static text nav_template = load_file(get_template_path("nav.html"));
-    text nav_items = create_text();
+std::string generate_nav(const std::string_view& site_name, const json& sites) {
+    static std::string nav_template = load_file(get_template_path("nav.html"));
+    std::string nav_items = "";
     for (auto& site: sites.items()) {
         if (site.key() != "index") {
             std::string edited_link = site.key();
@@ -199,42 +139,42 @@ text generate_nav(const std::string_view& site_name, const json& sites) {
             }
             std::string site_title = site.value()["title"];
             if (site.key() == site_name) {
-                nav_items->append(fmt::format("<span class=\"nav-item nav-current\"><a href=\"{}\" >{}</a>/<span>",
+                nav_items += fmt::format("<span class=\"nav-item nav-current\"><a href=\"{}\" >{}</a>/<span>",
                                               edited_link,
-                                              site_title));
+                                              site_title);
             } else {
-                nav_items->append(fmt::format("<span class=\"nav-item\"><a href=\"{}\" >{}</a>/<span>",
+                nav_items += fmt::format("<span class=\"nav-item\"><a href=\"{}\" >{}</a>/<span>",
                                               edited_link,
-                                              site_title));
+                                              site_title);
             }
         }
     }
-    text res = create_text();
-    res->append(fmt::format(nav_template->data(), fmt::arg("nav_items", nav_items->data())));
+    std::string res = "";
+    res += fmt::format(nav_template, fmt::arg("nav_items", nav_items));
     return res;
 }
 
-text generate_body(const std::string& site_name, const json& sites) {
+std::string generate_body(const std::string& site_name, const json& sites) {
     // const json site_data = sites[site_name];
-    text site_template = load_file(get_template_path(fmt::format("{}.html", site_name)));
-    text res = create_text();
-    res->append(fmt::format("{}\n{}", generate_nav(site_name, sites)->data(), site_template->data()));
+    std::string site_template = load_file(get_template_path(fmt::format("{}.html", site_name)));
+    std::string res = "";
+    res += fmt::format("{}\n{}", generate_nav(site_name, sites), site_template);
     return res;
 }
 
 void netmake::generate_simple_site(const std::string& site_name, const json& sites) {
     auto file = fmt::output_file(get_destination_path(fmt::format("{}.html", site_name)).c_str());
     file.print("<head>\n{}\n</head>\n<body>\n{}\n</body>\n",
-               generate_header(sites[site_name])->data(),
-               generate_body(site_name, sites)->data());
+               generate_header(sites[site_name]),
+               generate_body(site_name, sites));
 }
 
 void netmake::generate_complex_site(const std::string& site_name, const json& sites) {
     std::string file_name = site_name.substr(0, site_name.size() - 2);
     auto main_site = fmt::output_file(get_destination_path(fmt::format("{}.html", file_name)).c_str());
 
-    text main_site_template = load_file(get_template_path(fmt::format("{}.html", file_name)));
-    text nav_bar = generate_nav(site_name, sites);
+    std::string main_site_template = load_file(get_template_path(fmt::format("{}.html", file_name)));
+    std::string nav_bar = generate_nav(site_name, sites);
 
     auto list = sites[site_name]["items"];
     if (list.is_string()) {
@@ -242,27 +182,27 @@ void netmake::generate_complex_site(const std::string& site_name, const json& si
     }
 
     std::filesystem::create_directories(get_destination_path(file_name));
-    text generated_list = create_text();
+    std::string generated_list = "";
     if (list.size() > 0) {
         std::string item_template_name = sites[site_name]["list_item_template"];
-        text item_template = load_file(get_template_path(fmt::format("{}.html", item_template_name)));
+        std::string item_template = load_file(get_template_path(fmt::format("{}.html", item_template_name)));
         for (auto item: list) {
             std::string url = fmt::format("{}/{}.html", file_name, item["name"]);
-            generated_list->append(fmt::format(item_template->data(), fmt::arg("item_title", item["title"]),
-                                                                      fmt::arg("item_description", item["description"]),
-                                                                      fmt::arg("item_url", url)));
+            generated_list += fmt::format(item_template, fmt::arg("item_title", item["title"]),
+                                                         fmt::arg("item_description", item["description"]),
+                                                         fmt::arg("item_url", url));
             
             auto site = fmt::output_file(get_destination_path(url).c_str());
-            text site_template = load_file(get_template_path(fmt::format("{}.html", item["name"])));
+            std::string site_template = load_file(get_template_path(fmt::format("{}.html", item["name"])));
             site.print("<head>\n{}\n</head>\n<body>\n{}\n{}\n</body>\n",
-               generate_header(sites[site_name], item["title"])->data(),
-               nav_bar->data(),
-               fmt::format(site_template->data(), fmt::arg("generated_list", generated_list->data())));
+               generate_header(sites[site_name], item["title"]),
+               nav_bar,
+               fmt::format(site_template, fmt::arg("generated_list", generated_list)));
         }
     }
 
     main_site.print("<head>\n{}\n</head>\n<body>\n{}\n{}\n</body>\n",
-               generate_header(sites[site_name])->data(),
-               nav_bar->data(),
-               fmt::format(main_site_template->data(), fmt::arg("generated_list", generated_list->data())));
+               generate_header(sites[site_name]),
+               nav_bar,
+               fmt::format(main_site_template, fmt::arg("generated_list", generated_list)));
 }
