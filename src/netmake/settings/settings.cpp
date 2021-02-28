668344ba5e37ddc29eedce9515fa4af323280136
Tomas Vybiral <vybiraltomas@seznam.cz>
2021-02-28 16:41:13 +0000

    updated settings

--- src/netmake/settings/settings.cpp
+++ src/netmake/settings/settings.cpp
@@ -4,7 +4,6 @@ using namespace netmake;
 
 std::filesystem::path settings::source_dir{"./src"};
 std::filesystem::path settings::dest_dir{"./generated"};
-bool settings::concat_nav{true};
 
 void settings::init(const std::vector<std::string>& args) {
     for (size_t i = 0; i < args.size(); i++) {
@@ -18,6 +17,25 @@ void settings::init(const std::vector<std::string>& args) {
         } else if (arg == "-C" || arg == "--unique_navigations") {
             settings::concat_nav = false;
         }
-
     }
+}
+
+settings settings::master() noexcept {
+    return settings(settings::source_dir, settings::dest_dir);
+}
+
+std::filesystem::path settings::path_to_source_file(const std::string_view file_name) const noexcept {
+    return source / std::filesystem::path{file_name.data()};
+}
+
+std::filesystem::path path_to_template_file(const std::string_view file_name) const noexcept {
+    return source / std::filesystem::path{"templates"} / std::filesystem::path{file_name.data()};
+}
+
+std::filesystem::path path_to_destination_file(const std::string_view file_name) const noexcept {
+    return destination / std::filesystem::path{file_name.data()};
+}
+
+void settings::offset(const std::string_view subfolder) noexcept {
+    destination /= std::filesystem::path(subfolder.c_str());
 }
--- src/netmake/settings/settings.hpp
+++ src/netmake/settings/settings.hpp
@@ -1,16 +1,33 @@
 #ifndef NETMAKE_SETTINGS_HPP
 #define NETMAKE_SETTINGS_HPP
 
-#include <string>
+#include <string_view>
 #include <vector>
 #include <filesystem>
 
 namespace netmake {
     struct settings {
-        static void init(const std::vector<std::string>& args);
+        /// Processes user arguments and reinitiliazes the settings values
+        static void init(const std::vector<std::string>& args) noexcept;
+        /// Get local copy of master settings
+        static [[nodiscard]] settings master() noexcept;
+        /// Source file folder location
         static std::filesystem::path source_dir;
+        /// Destination folder, where generated data are stored.
         static std::filesystem::path dest_dir;
-        static bool concat_nav;
+
+        /// Get path to source file defined by file_name
+        [[nodiscard]] std::filesystem::path path_to_source_file(const std::string_view file_name) const noexcept;
+        /// Get path to template file in source directory
+        [[nodiscard]] std::filesystem::path path_to_template_file(const std::string_view file_name) const noexcept;
+        /// Get path for output file
+        [[nodiscard]] std::filesystem::path path_to_destination_file(const std::string_view file_name) const noexcept;
+        /// Offset local copy of settings into defined subfolder.
+        void offset(const std::string_view subfolder) noexcept;
+    protected:
+        settings(const std::filesystem::path& source_dir, const std::filesystem::path& destination_dir);
+        std::filesystem::path source;
+        std::filesystem::path destination;
     };
 }
 
